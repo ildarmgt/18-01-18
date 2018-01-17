95cdbdf6d03dd4082248c927c76f42622af2cfb5
ildarmgt <ildarmgt@gmail.com>
2018-01-17 17:30:54 -0500

    finished everything

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// ----------- part 1: buttons -------------
+
 var btnLeft = document.getElementsByClassName('mainNav-divLeft-btn')[0]
 var btnRight = document.getElementsByClassName('mainNav-divRight-btn')[0]
 
@@ -19,5 +21,36 @@ btnRight.addEventListener('click', function () {
     txtRight.innerHTML = 'I\'m right'
   } else {
     txtLeft.innerHTML = 'No, I\'m right!'
+  }
+})
+
+// ----------- part 2: hover -------------
+
+var divHover = document.getElementsByClassName('mainNav-divHover')[0]
+
+divHover.addEventListener('mouseenter', function () {
+  alert('Hey, I told you not to hover over me!')
+})
+
+// ----------- part 3: key press -------------
+
+var h1Key = document.getElementsByClassName('mainNav-h1Key')[0]
+
+document.addEventListener('keypress', function (event) {
+  h1Key.innerHTML = event.key
+})
+
+// ----------- part 4: login -------------
+
+var btnSubmit = document.getElementsByClassName('mainNav-btnSubmit')[0]
+var txtUser = document.getElementsByClassName('mainNav-txtUser')[0]
+var txtPass = document.getElementsByClassName('mainNav-txtPass')[0]
+var h1Login = document.getElementsByClassName('mainNav-h1Login')[0]
+
+btnSubmit.addEventListener('click', function () {
+  if ((txtPass.value === '12345678') && (txtUser.value.length < 14)) {
+    h1Login.innerHTML = 'Congrats on knowing your username and password!'
+  } else {
+    alert('Incorrect username or password')
   }
 })
